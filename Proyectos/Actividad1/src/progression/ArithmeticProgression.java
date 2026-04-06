bc6ead9ca2a14513ea7b51059e76d5d7b90ff081
josenatteschoque <josenatteschoque@gmail.com>
2026-04-05 21:39:17 -0300

    Primer commit

---
+++ Proyectos/Actividad1/src/progression/ArithmeticProgression.java
@@ -0,0 +1,19 @@
+package progression;
+
+public class ArithmeticProgression extends Progression{
+	
+	protected long increment;
+	
+	public ArithmeticProgression() { this(0);}
+	
+	public ArithmeticProgression(long stepsize) { this(stepsize, 0);}
+	
+	public ArithmeticProgression(long stepsize, long start) {
+		super(start);
+		increment = stepsize;
+	}
+	
+	protected void advance() {
+		current += increment;
+	}
+}
---
+++ Proyectos/Actividad1/src/progression/GeometricProgression.java
@@ -0,0 +1,18 @@
+package progression;
+
+public class GeometricProgression extends Progression{
+
+	protected long base;
+	
+	public GeometricProgression() { this(2, 1);}
+	
+	public GeometricProgression(long b, long start) {
+		super(start);
+		base = b;
+	}
+	
+	protected void advance() {
+		current *= base;
+	}
+	
+}
