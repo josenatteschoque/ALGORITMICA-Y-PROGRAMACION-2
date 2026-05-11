99ef3e040cd78b9500d698580b4d8a61c2e33ee1
josenatteschoque <josenatteschoque@gmail.com>
2026-05-10 21:52:14 -0300

    Metodos aplicados!

--- Tp3/Listas/src/net/datastructures/SinglyLinkedList.java
+++ Tp3/Listas/src/net/datastructures/SinglyLinkedList.java
@@ -160,6 +160,7 @@ public class SinglyLinkedList<E> implements Cloneable {
     return answer;
   }
 
+
   @SuppressWarnings({"unchecked"})
   public boolean equals(Object o) {
     if (o == null) return false;
@@ -219,4 +220,302 @@ public class SinglyLinkedList<E> implements Cloneable {
     sb.append(")");
     return sb.toString();
   }
+  
+  /* Inserta el elemento e en la posicion n de la lista */	
+  //public void addPos(E e, int n) throws IndexOutOfBoundsException Metodo que inserta el elemento en la posicion n
+  public void addPos(E e, int n) throws IndexOutOfBoundsException{
+	  //Muestro estos mensajes de excepciones 
+	  if(n < 0 ) throw new IndexOutOfBoundsException("La posicion no puede ser negativa!");
+	  if(n > size) throw new IndexOutOfBoundsException("La posicion no puede ser mayor al tamaño de la lista");
+	  if(n == 0) {	//En caso de que sea la primer posicion 
+		  addFirst(e);
+		  }	
+	  else if(n == size) {	//En caso de que sea la ultima posicion 
+		  addLast(e);
+	  }else {
+		  Node<E> actual = head;
+		  Node<E> anterior = null;
+		  int c = 0;
+		  while(c < n) {
+			  anterior = actual;
+			  actual = actual.getNext();
+			  c++;
+		  }
+		  Node<E> nuevo = new Node<>(e, actual);	//Creo un nuevo Node
+		 anterior.setNext(nuevo);
+		 size++;
+	  }
+  }
+  //Funciona siuuuu :)
+  
+  /* Elimina el elemento e de la lista 
+  /* Retorna NULL si no lo encuentra */
+  //public E removeElement(E e) Metodo que sirve para eliminar un elemento en la lista
+  public E removeElement(E e) {
+	  	if(isEmpty()) return null;	//si la lista esta vacia retorna null
+	  	Node<E> walk = head;	
+	  	Node<E> anterior = null;  
+	  	
+	  	while(walk != null) {	//Recorre la lista
+	  		if(walk.getElement().equals(e)) {
+	  			
+	  			if(anterior == null) {
+	  				head = walk.getNext();
+	  			}else {
+	  				anterior.setNext(walk.getNext());
+	  			}
+	  			size--;
+	  		}
+	  		anterior = walk;
+	  		walk = walk.getNext();	//Avanza al siguiente elemento
+	  	}
+	  	return null;	//Si no esta el elemento en la lista retorna null
+  }
+  //Funciona siuuuu :)
+  
+
+  /* Elimina elemento que se encuentra en la posicion n de la lista */
+  /* Retorna NULL si no es una posición valida */
+  //public E removePos(int n) throws IndexOutOfBoundsException Metodo que sirve para eliminar un elemento en la posicion n de la lista
+  public E removePos(int n) throws IndexOutOfBoundsException{
+	//Muestro estos mensajes de excepciones 
+	  if(n < 0 ) throw new IndexOutOfBoundsException("La posicion no puede ser negativa!");
+	  if(n > size) throw new IndexOutOfBoundsException("La posicion no puede ser mayor al tamaño de la lista");
+	  if(isEmpty()) return null;
+
+	  Node<E> walk = head;
+	  Node<E> anterior = null;
+	  int c = 0;
+	  
+	  //Recorro la lista hasta encontrar la posicion del elemento a eliminar 
+	  while(c < n) {
+		  anterior = walk;
+		  walk = walk.getNext();
+		  c++;
+	  }
+	  anterior.setNext(walk.getNext());	//Apunto la referencia al siguiente elemento haci eliminando el deseado
+	  return null;	//Si el elemento no esta en la lista retorna null
+  }	
+  //Funciona siuuuuuu :)
+
+  
+	
+/* Inserta todos los elementos de la Lista l al final de la lista */
+//public void concatenate(SinglyLinkedList<E> l) Metodo que sirve para insertar todos los elementos de una lista al final de otro lista
+  public void concatenate(SinglyLinkedList<E> lista2) {
+	  //Si la lista esta vacia no modifica lista1 
+	  if(lista2.isEmpty()) {
+		  return;
+	  }
+	  
+	  //Si la lista1 esta vacia, las referencias de la lista1 apuntan a lista2
+	  if(isEmpty()) {
+		  this.head = lista2.head;
+		  this.tail = lista2.tail;
+		  this.size = lista2.size;	
+		  return;
+	  }
+	  
+	  //Apunta las referencias ala siguiente lista
+	  this.tail.setNext(lista2.head);
+	  this.tail = lista2.tail;
+	  this.size += lista2.size; 
+  }
+//Funciona siuuuuu
+  
+  /* Busca el elemento e dentro de la lista */
+  /* Retorna el elemento si lo encuentra o Null si no está en la lista */
+  //public E search(E e) Metodo que sirve para buscar un elemento en la lista
+  public E search(E e) {
+	  if(isEmpty()) return null;	//En caso de que la lista este vacia retorna null
+	  
+	  Node<E> walk = head;	//Creo un nuevo Node que apunte a head 
+	  
+	  while(walk != null) {	//Recorro la lista hasta que termine 
+		  if(walk.getElement().equals(e)) {	//Ve si el elemento actual es igual a e
+			  return walk.getElement();	//Si son iguales retorna el elemento
+		  }
+		  walk = walk.getNext();	//Avanza al siguiente
+	  }
+	  return null;	//Si no lo encontro en la lista retorna null
+  }//Funcionaaaa
+  
+  
+  //public void addCombineAfter(SinglyLinkedList<E> l) Metodo que sirve para para combinar una lista con otro de forma cruzada osea lista1 ={a,b,c} y lista2 = {1,2,3}
+  //seria {a1,b2,c3}
+  public void addCombineAfter(SinglyLinkedList<E> l) {
+	  if(l.isEmpty()) return;
+	  
+	  //Si la lista original esta vacia tomamos toda la lista l
+	  if(this.isEmpty()) {
+		  this.head = l.head;
+		  this.tail = l.tail;
+		  this.size = l.size;
+		  return;
+	  }
+	  
+	  
+	  Node<E> c1 = this.head;
+	  Node<E> c2 = l.head;
+	  
+	  while(c1 != null && c2 != null) {
+		  
+		  Node<E> siguiente1 = c1.getNext();
+		  Node<E> siguiente2 = c2.getNext();
+		  
+		  c1.setNext(c2);
+		  
+		  if(siguiente1 == null) {
+			  this.tail = l.tail;
+			  break;
+		  }
+		  
+		  c2.setNext(siguiente1);
+		  
+		  //Avanzo los punteros
+		  c1 = siguiente1;
+		  c2 = siguiente2;
+	  }
+	  this.size += l.size;
+  }
+ //Funciona siuuu :)
+  
+  
+//public void addCombineBefore(SinglyLinkedList<E> l) Metodo que sirve para
+	// combinar una lista con otra pero antes de primer elemento ejemplo lista1 =
+	// {a,b,c} y lista2 = {x,y,z}
+	// {xa, yb, zc}
+	public void addCombineBefore(SinglyLinkedList<E> l) {
+		 if(l.isEmpty()) return;
+		  
+		  //Si la lista original esta vacia tomamos toda la lista l
+		  if(this.isEmpty()) {
+			  this.head = l.head;
+			  this.tail = l.tail;
+			  this.size = l.size;
+			  return;
+		  }
+		  
+		  Node<E> c2 = l.head;
+		  Node<E> c1 = this.head;
+		  
+		  this.head = c2;
+		  
+		  while(c1 != null && c2 != null) {
+			  Node<E> next2 = c2.getNext();
+			  Node<E> next1 = c1.getNext();
+			  
+			  c2.setNext(c1);
+			  
+			  if(next2 == null) {
+				  break;
+			  }
+			  
+			  if(next1 == null) {
+				  c1.setNext(next2);
+				  this.tail = l.tail;
+				  break;
+			  }
+			  
+			  c1.setNext(next2);
+			  
+			  c2 = next2;
+			  c1 = next1;
+		  }
+		  this.size += l.size;
+	}
+//Funciona siuu :)
+	
+	
+	/**
+	 * Elimina todos los elementos que están en una posición impar. 
+	 * Retorna una lista con los elementos eliminados
+	 *
+	 * Por ejemplo:
+	 *
+	 * {A, B, C, D} => {A, C} retorna la lista {B, D}
+	 *
+	 * {A, B, C} => {A, C} retorna la lista {B}
+	 *
+	 * {A, B} => {A} retorna la lista {B}
+	 * 
+	 * {A} => {A} retorna la lista {} 
+	 * 
+	 * {} => {} retorna la lista {} 
+	 */
+	public SinglyLinkedList<E> removeOdd(){
+		SinglyLinkedList<E> removeList = new SinglyLinkedList<>();	//Lista donde estaran los elementos de posiciones impares
+		
+		if(head == null || head.getNext() == null) {
+			return  removeList;	//Si la lista esta vacia o solo tiene un elemento 
+		}
+		
+		Node<E> walk = head;
+		while(walk != null) {
+			
+			Node<E>siguiente = walk.getNext();
+			
+			removeList.addLast(siguiente.getElement());	//Agrego el elemento en la nueva lista
+			
+			walk.setNext(siguiente.getNext());	//Salto al siguiente elemento
+		
+			if(walk.getNext() == null) {	//actualizo el tail
+				tail = walk;
+			}
+			size--;	//Declementa el tamaño de la lista	
+			walk = walk.getNext();	//Avanza al siguiente elemento
+		}
+		return removeList;	//Retorna la lista de elementos eliminados
+	}
+//Funciona siuuu :)
+	
+	/**
+	 * Retorna una nueva lista con los n elementos comenzado desde la derecha
+	 * 
+	 * Por ejemplo:
+	 * 
+	 * Dada la lista {A, B, C, D}
+	 * 
+	 * right(4) retorna la lista {A, B, C, D}
+	 * 
+	 * right(3) retorna la lista {B, C, D}
+	 * 
+	 * right(2) retorna la lista {C, D}
+	 * 
+	 * right(1) retorna la lista {D}
+	 * 
+	 * right(0) retorna la lista {}
+	 * 
+	 * right(5) lanza la excepción IndexOutOfBoundsException
+	 * 
+	 * right(-1) lanza la excepción IndexOutOfBoundsException
+	 * 
+	 * 
+	 * @param n: número de elementos a retornar comenzando desde la derecha
+	 * @return nueva lista con los elementos de la derecha de la lista original
+	 * @throws IndexOutOfBoundsException n: supera el tamaño de la lista o es negativo
+	 */
+	public SinglyLinkedList<E> right(int n) throws IndexOutOfBoundsException{
+		//Mensaja de excepcion
+		if(n < 0 || n > size) throw new IndexOutOfBoundsException("N supera el tamaño el tamaño de la lista o es negativo!");
+		SinglyLinkedList<E> nuevalista = new SinglyLinkedList<>();
+		if(n == 0) return nuevalista;
+		int saltos = size - n;
+		Node<E> walk = head;
+
+		//Avanza hasta la posicion necesaria
+		for(int i = 0; i < saltos; i++) {
+			walk = walk.getNext();
+		}
+		
+		//Agrega los elementos en al nueva lista
+		while(walk != null) {
+			nuevalista.addLast(walk.getElement());
+			walk = walk.getNext();
+		}
+		//Retorna la nueva lista
+		return nuevalista;
+	}
+
+
 }
