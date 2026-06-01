10f9a59b1fead3f6c124d0bae0d94c059abe95e6
josenatteschoque <josenatteschoque@gmail.com>
2026-06-01 18:10:45 -0300

    Ejercicio Finalizado!

--- Tp5/Ejercicio9/src/net/datastructures/ArrayCircularDeque.java
+++ Tp5/Ejercicio9/src/net/datastructures/ArrayCircularDeque.java
@@ -1,17 +1,19 @@
 package net.datastructures;
 
+import java.util.Arrays;
+
 public class ArrayCircularDeque<E> implements Deque<E> {
-	private int capasyte;
+	private int capacity;
 	private E[] array;
 	private int size;
-	private int inicio;
+	private int frente;
 	private int fin;
 	
 	
 	public ArrayCircularDeque(int capacity) {
-		this.capasyte = capasyte;
+		this.capacity = capacity;
 		array = (E[]) new Object[capacity];
-		inicio = 0;
+		frente = 0;
 		fin = -1;
 		size = 0;
 	}
@@ -32,7 +34,7 @@ public class ArrayCircularDeque<E> implements Deque<E> {
 	@Override
 	public E first() {
 		if(isEmpty()) return null;
-		return array[inicio];
+		return array[frente];
 	}
 
 	//Retorna el ultimo elemento
@@ -42,33 +44,77 @@ public class ArrayCircularDeque<E> implements Deque<E> {
 		return array[fin];
 	}
 
-	//Agrega ala primera posicion 
+	//Agrega ala ultima posicion 
 	@Override
 	public void addFirst(E e) {
-		inicio = (inicio -1 + array.length)% array.length;
-		array[inicio++] = e;
+		//Esto hace que vuelva ala ultima posicion del arreglo
+		frente = (frente - 1 + array.length ) % array.length;
+		array[frente] = e;
 		size++;
 	}
 
-	//Agrega ala ultima posicion
+	//Agrega ala primera posicion
 	@Override
 	public void addLast(E e) {
+		//Esto hace que agregue el elemento en la primer posicion del arreglo
 		fin = (fin + 1) % array.length;
-		array[fin++] = e;
+		array[fin] = e;
 		size++;
 	}
 
-	//Elimina el elemento de la primer posicion
+	//Elimina el elemento del frente
 	@Override
 	public E removeFirst() {
-		return null;
+		//Si el arreglo esta vacio retorna null
+		if(isEmpty()) return null;
+		
+		//Guardo el elemento a eliminar
+		E element = array[frente];
+		
+		//Limpio la posicion del elemento eliminado
+		array[frente] = null;
+		
+		//Muevo el frente hacia adelante
+		frente = (frente + 1 ) % array.length;
+		
+		//Declemento el tamaño
+		size--;
+		
+		//Retorno el elemento eliminado
+		return element;
 	}
 
-	//Elimina el elemento de la ultima posicion
+	//Elimina el elemento del fin
 	@Override
 	public E removeLast() {
-		return null;
+		//Si el arreglo esta vacio retorna null
+		if(isEmpty()) return null;
+		
+		//Guardo el elemento a eliminar
+		E element = array[fin];
+		
+		//Limpio la posicion del elemento eliminado
+		array[fin] = null;
+		
+		//Muevo el fin
+		fin = (fin - 1 + array.length) % array.length;
+		
+		//Declemento el tamaño 
+		size--;
+		
+		//Retorno el elemento eliminado
+		return element;
 	}
+
+	@Override
+	public String toString() {
+		return "ArrayCircularDeque [capacity=" + capacity + ", array=" + Arrays.toString(array) + ", size=" + size
+				+ ", frente=" + frente + ", fin=" + fin + "]";
+	}
+
+	
+	
+	
 	
 	
 }
--- Tp5/Ejercicio9/src/test/TestArrayCircularDeque.java
+++ Tp5/Ejercicio9/src/test/TestArrayCircularDeque.java
@@ -6,13 +6,21 @@ import net.datastructures.ArrayCircularDeque;
 public class TestArrayCircularDeque {
 
 	public static void main(String[] args) {
-		Deque<String> manolo = new ArrayCircularDeque<>();
+		Deque<String> deque = new ArrayCircularDeque<>(5);
 		
-		manolo.addFirst("Leo");
-		manolo.addLast("Fiona");
-		System.out.println("Tamaño: "+manolo.size());
-		System.out.println(manolo.first());
-		System.out.println(manolo);
+		deque.addFirst("Leo");
+		deque.addLast("Fiona");
+		deque.addFirst("Jose");
+		deque.addLast("Kira");
+		
+		System.out.println("Tamaño: "+deque.size());
+		System.out.println("Array: "+deque);
+		System.out.println("Primero: "+deque.first());
+		
+		System.out.println("Primero eliminado: "+deque.removeFirst());
+		System.out.println("Ultimo eliminado: "+deque.removeLast());
+		
+		System.out.println("Array: "+deque);
 
 		
 	}
