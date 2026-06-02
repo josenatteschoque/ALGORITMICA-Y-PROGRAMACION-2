a026e3a15d3c6b18b5a685b10b797d45ebe86047
josenatteschoque <josenatteschoque@gmail.com>
2026-06-02 12:50:06 -0300

    Ejemplo en Clase!

---
+++ Tp7/ClaseArbol/src/test/TestArbol.java
@@ -0,0 +1,51 @@
+package test;
+
+import net.datastructures.LinkedBinaryTree;
+import net.datastructures.Position;
+
+public class TestArbol {
+
+	public static void main(String[] args) {
+		LinkedBinaryTree<String> t = new LinkedBinaryTree<>();
+
+		Position<String> p, q;
+		p = t.addRoot("A");
+
+		q = t.addLeft(p, "B");
+
+		t.addRight(p, "C");
+
+		t.addLeft(q, "D");
+
+		t.addRight(q, "E");
+
+		System.out.println(t);
+		
+		//Formas de recorrer un arbol
+
+		// PreOrden RID = raiz, izquierda, derecha
+		//A,B,D,E,C
+		
+		for (Position<String> r : t.preorder()) { 
+			 System.out.println("Preorden: " +r.getElement()); 
+		}
+		System.out.println();
+		 
+		// InOrden IRD = izquierda, raiz, derecha
+		//D,B,E,A,C
+		for (Position<String> r : t.inorder()) {
+			System.out.println("Inorden: " + r.getElement());
+		}
+		System.out.println();
+
+
+		// PostOrden IDR = izquierda, derecha, raiz
+		//D,E,B,C,A
+		for (Position<String> r : t.postorder()) {
+			System.out.println("Postorden: " + r.getElement());
+		}
+		System.out.println();
+
+	}
+
+}
