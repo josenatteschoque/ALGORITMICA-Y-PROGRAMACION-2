fb7ea9b20e57292b4856dbddfc270c9d2a074304
josenatteschoque <josenatteschoque@gmail.com>
2026-06-06 00:57:54 -0300

    Ejercicio Incompleto!

---
+++ Tp4/Ejercicio6/src/quicksortrecursivo/QuicksortRecursivo.java
@@ -0,0 +1,5 @@
+package quicksortrecursivo;
+
+public class QuicksortRecursivo {
+	
+}
---
+++ Tp4/Ejercicio6/src/quicksortrecursivo/TestQuickSortRecursivo.java
@@ -0,0 +1,18 @@
+/*6. Realizar una implementación recursiva utilizando el método de ordenamiento quicksort para ordenar un array de enteros.
+ *  Realizar una traza del algoritmo e indicar su complejidad.*/
+package quicksortrecursivo;
+
+public class TestQuickSortRecursivo {
+	public static void main(String[] args) {
+		int array[] = {1,22,13,10,5,6};
+
+		//Tomo el tiempo de inicio
+		long inicio = System.nanoTime();
+
+		//Tomo el tiempo final
+		long fin = System.nanoTime();
+		
+		System.out.println("");
+		
+	}
+}
